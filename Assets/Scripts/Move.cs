0c8b4654fe681522a6405df3dee9456222af6f26
ProSleeper <ingngnin@gmail.com>
2017-03-06 00:27:09 +0900

    First

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	bool isMove;
+
+	// Use this for initialization
+	void Start () {
+		isMove = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			isMove = false;
+		}
+		
+		if (isMove)
+		{
+			this.transform.Translate(new Vector3(0.0f, 0.0f, -3.0f * Time.deltaTime));
+		}
+		
+	}
+}
